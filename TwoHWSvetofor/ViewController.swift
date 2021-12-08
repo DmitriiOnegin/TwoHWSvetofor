2ed4c571e6600da63f74725251ec07d71b45812a
Dmitrii Onegin <dimimimo@mail.ru>
2021-12-08 21:46:42 +0300

    Add logic

--- TwoHWSvetofor/ViewController.swift
+++ TwoHWSvetofor/ViewController.swift
@@ -14,13 +14,62 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var buttonLable: UIButton!
     
+    var num = 0
+    
+//    enum Lamp {
+//        case nill
+//        case red
+//        case yellow
+//        case green
+//
+//        var activ {
+//
+//        }
+//    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        redLamp.layer.cornerRadius = 40
+        yellowLamp.layer.cornerRadius = 40
+        greenLamp.layer.cornerRadius = 40
+        redLamp.alpha = 0.3
+        yellowLamp.alpha = 0.3
+        greenLamp.alpha = 0.3
+        buttonLable.setTitle("START", for: .normal)
     }
 
     @IBAction func buttonAction() {
+        buttonLable.setTitle("NEXT", for: .normal)
+        if num < 3 {
+            num += 1 } else {
+                num = 1
+            }
+                
+        switch num {
+        case 0:
+            redLamp.alpha = 0.3
+            yellowLamp.alpha = 0.3
+            greenLamp.alpha = 0.3
+        case 1:
+            redLamp.alpha = 1
+            yellowLamp.alpha = 0.3
+            greenLamp.alpha = 0.3
+        case 2:
+            redLamp.alpha = 0.3
+            yellowLamp.alpha = 1
+            greenLamp.alpha = 0.3
+        case 3:
+            redLamp.alpha = 0.3
+            yellowLamp.alpha = 0.3
+            greenLamp.alpha = 1
+        default:
+         return
+        }
+        
     }
+    
+    
     
 }
 
